fa3b780612713764fa36dd633e7e44d4f48d1854
Freddie Sackur <Freddie.Sackur@rackspace.co.uk>
2017-07-15 10:43:48 +0100

    Uses a separate runspace

--- Public/Get-RunObject.ps1
+++ Public/Get-RunObject.ps1
@@ -11,13 +11,21 @@ function Get-RunObject {
     $RunObj = New-Object psobject -Property @{
         Script = $Script;
         Dependencies = $Dependencies;
-        ConnectionInfo = $ConnectionInfo
+        ConnectionInfo = $ConnectionInfo;
+        PS = [powershell]::Create();
+        RS = [runspacefactory]::CreateRunspace()
     }
+    
+    $RunObj.PS.Runspace = $RunObj.RS
+    $null = $RunObj.PS.AddScript($RunObj.Script)
+    $RunObj.RS.Open()
 
     $RunObj | Add-Member -MemberType ScriptMethod -Name Invoke -Value {
         [CmdletBinding()]
         param()
-        Write-Output "You invoked, sir?"
+        $Output = $RunObj.PS.Invoke()
+        $Output
+        $RunObj.RS.Dispose()
     }
 
     return $RunObj
