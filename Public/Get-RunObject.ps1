6040aa881e00d18f8a7d092386e49cc196e4ab8b
Freddie Sackur <Freddie.Sackur@rackspace.co.uk>
2017-07-15 12:59:05 +0100

    Asynchronous runspace execution

--- Public/Get-RunObject.ps1
+++ Public/Get-RunObject.ps1
@@ -36,23 +36,41 @@ function Get-RunObject {
 
     $RunObj.RS.Name = $ConnectionInfo.DeviceID
     $RunObj.PS.Runspace = $RunObj.RS
+    if ($Debug) {$null = $RunObj.PS.AddScript('Wait-Debugger;')}
     $null = $RunObj.PS.AddScript($RunObj.Script)
     $RunObj.RS.Open()
     if ($Debug) {
         $RunObj.RS.SessionStateProxy.SetVariable("ELF_DEBUG", $Debug)
     }
 
+    $RunObj | Add-Member -MemberType ScriptMethod -Name Invoke -Value {
+        [CmdletBinding()]
+        param()
+
+        $Output = $RunObj.PS.Invoke()
+        $Output
+        $RunObj.RS.Dispose()
+    }
+
     $RunObj | Add-Member -MemberType ScriptMethod -Name BeginInvoke -Value {
         [CmdletBinding()]
         param()
 
-        $Output = $RunObj.PS.BeginInvoke()
+        $Handle = $RunObj.PS.BeginInvoke()
+        
+        #Should be event-driven, sleep is just for testing
+        while (-not $Handle.IsCompleted) {
+            Start-Sleep -Milliseconds 200
+        }
+        
+        $Output = $RunObj.PS.EndInvoke($Handle)
         $Output
-        #$RunObj.RS.Dispose()
+        $RunObj.RS.Dispose()
     }
 
     return $RunObj
 }
 
-$RunObj = Get-RunObject -Script 'Wait-Debugger; return $ELF_DEBUG' -Debug -Verbose
+$RunObj = Get-RunObject -Script 'Start-Sleep 1; return $ELF_DEBUG' -Debug -Verbose
 $RunObj.BeginInvoke()
+#$RunObj.Invoke()
